1009223e0a67ef211164561f39b10b2c8f3cb986
Peter Radko <cmtvpetya@gmail.com>
2026-02-09 10:13:33 +0300

    News item about FAQ page

---
+++ news/2026.02.08.tsx
@@ -0,0 +1,8 @@
+export default (
+  <>
+    <P>
+      A new <Ref to={$CONTENT.faq}>intro page</Ref> about the "Open Math" project mission, its principles, and how to
+      use it to learn math most effectively.
+    </P>
+  </>
+);
